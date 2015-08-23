eb3055e3687730d861733ef57aa4bc4007bc7177
Nathachai Thongniran <inid3a@gmail.com>
2015-08-23 18:30:31 +0700

    Init project

---
+++ auto-subpage-menu.php
@@ -0,0 +1,10 @@
+/*
+Plugin Name: Auto Subpage Menu
+Plugin URI: https://github.com/jojoee/auto-subpage-menu
+Description: New child pages will be automatically added to menus,
+	update the menu hierarchy when page's updated and
+	also remove page from menu when page's moved to trash
+Version: 0.0
+Author: Nathachai Thongniran
+Author URI: http://jojoee.com/
+*/
